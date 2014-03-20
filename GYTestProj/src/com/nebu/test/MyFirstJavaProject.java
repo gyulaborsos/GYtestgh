ace689a524edbb7e1115877ca988654d08a23825
Gyula.Borsos <Gyula.Borsos@nebu.com>
2014-03-20 15:49:18 +0100

    testclass

---
+++ GYTestProj/src/com/nebu/test/MyFirstJavaProject.java
@@ -0,0 +1,11 @@
+package com.nebu.test;
+
+public class MyFirstJavaProject {
+
+	public static void main(String[] args) {
+		// TODO unimplemented method
+		throw new UnsupportedOperationException("Not implemented yet.");
+
+	}
+
+}
